738c7ee2155fcf3583d5eaeb5ea3b4148443c811
simoneburrai <burrai.s98@gmail.com>
2025-04-25 01:10:06 +0200

    feat: created winning and tie functions, implemented in tris games, game completed and working

--- src/pages/TrisGame.jsx
+++ src/pages/TrisGame.jsx
@@ -1,28 +1,72 @@
 import { useEffect, useState } from "react"
+
+const winningCombinations = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // Righe
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // Colonne
+    [0, 4, 8], [2, 4, 6]             // Diagonali
+];
+
 function Tris() {
 
     const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""])
-    const [playerTurn, setPlayerTurn] = useState(true);
-
+    const [playerTurn, setPlayerTurn] = useState(null);
+    const [playing, setPlaying] = useState(false);
+    const [result, setResult] = useState(null);
     const randomNumber = () => Math.floor(Math.random() * 9);
+    const computer = "O";
+    const player = "X"
 
+    const checkWin = (board, player) => {
+        return winningCombinations.some(combination => {
+            return combination.every(index => board[index] === player);
+        });
+    }
 
-
-    const changeValue = (i) => {
-
-        setBoard(() => {
-            const newArr = [...board];
-            newArr[i] = playerTurn ? "X" : "O"
-            return newArr;
-        })
-
-        setPlayerTurn(prev => !prev);
+    const checkTie = (board) => {
+        return board.every(box => box != "");
     }
 
 
+
+
+    const playFunction = () => {
+        setPlayerTurn(true);
+        setPlaying(true);
+        setBoard(["", "", "", "", "", "", "", "", ""]);
+        setResult(null);
+    }
+
+    const changeValue = (i) => {
+        if (playing && board[i] == "" && playerTurn) {
+            setBoard(() => {
+                const newArr = [...board];
+                newArr[i] = "X";
+                return newArr;
+            })
+
+            setPlayerTurn(prev => !prev);
+        }
+    }
+
+    useEffect(() => {
+        if (playing) {
+            if (checkWin(board, computer)) {
+                setResult("Hai perso");
+                setPlaying(false);
+            } else if (checkWin(board, player)) {
+                setResult("Hai Vinto");
+                setPlaying(false);
+            } else if (checkTie(board)) {
+                setResult("Pareggio");
+                setPlaying(false);
+            }
+        }
+
+    }, [board, playing])
+
     useEffect(() => {
         let timeOut;
-        if (!playerTurn) {
+        if (!playerTurn && playing) {
             timeOut = setTimeout(() => {
                 const newArr = [...board];
                 let randomId = randomNumber();
@@ -39,13 +83,16 @@ function Tris() {
                 clearTimeout(timeOut);
             }
         }
-    }, [playerTurn, board])
+    }, [playerTurn, board, playing])
 
 
 
     return (
         <>
+
             <div className="board">{board.map((value, index) => <button key={index} onClick={() => changeValue(index)}>{value}</button>)}</div>
+            <button onClick={playFunction}>PLAY</button>
+            {result && <div>{result}</div>}
         </>
     )
 }
