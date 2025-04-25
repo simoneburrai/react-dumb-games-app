1496143ba4cb03ceba801e7b9bc210b1aadb67b7
Carlo Costa <carlo.costa92@gmail.com>
2025-04-25 09:01:47 +0200

    feat: create home page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+const HomePage = () => {
+    return (
+        <div>
+            <h1>REACT DUMB GAMES APP</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas, quod tempore voluptatem blanditiis molestias, excepturi quo voluptate eius perspiciatis veniam voluptatum maiores eos quidem deleniti id error repellat cumque nesciunt!</p>
+        </div>
+    )
+}
+
+export default HomePage;
