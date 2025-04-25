1e1453607cff4bedd30abd802af9f56378e2e2e6
Carlo Costa <carlo.costa92@gmail.com>
2025-04-25 13:25:25 +0200

    feat: create route path

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import DiceGame from "./pages/DiceGame"
-import PaperScissorRock from "./pages/PaperScissorRock"
-import SecretNumber from "./pages/SecretNumberGame"
-import Tris from "./pages/TrisGame"
+import React from "react"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './layout/NavBar';
+import HomePage from './pages/HomePage';
+import DiceGame from './pages/DiceGame';
+import PaperScissorRock from './pages/PaperScissorRock';
+import TrisGame from './pages/TrisGame';
+import SecretNumberGame from './pages/SecretNumberGame';
 
 function App() {
 
 
   return (
-    <>
-
-      <DiceGame />
-      <Tris />
-      <PaperScissorRock />
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/Dice-game" element={<DiceGame />} />
+        <Route path="/Paper-Scissor-Rock" element={<PaperScissorRock />} />
+        <Route path="/Tris-game" element={<TrisGame />} />
+        <Route path="/Secret-Number" element={<SecretNumberGame />} />
+      </Routes>
+    </Router>
   )
 }
 
