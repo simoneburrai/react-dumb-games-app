d30bf2ab65c2aea6a1b353c6dd17aa4cef54b1b7
simoneburrai <burrai.s98@gmail.com>
2025-04-30 11:26:14 +0200

    feat: navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,6 @@ const Navbar = () => {
         <nav>
             <ul>
                 <li>
-<<<<<<< HEAD
                     <NavLink to="/">HomePage</NavLink>
                 </li>
                 <li>
@@ -19,21 +18,6 @@ const Navbar = () => {
                 </li>
                 <li>
                     <NavLink to="/Secret-Number">Secret Number Game</NavLink>
-=======
-                    <Link to="/" className='navbar'>HomePage</Link>
-                </li>
-                <li>
-                    <Link to="/Dice-game" className='navbar'>Dice Game</Link>
-                </li>
-                <li>
-                    <Link to="/Paper-Scissor-Rock" className='navbar'>Paper Scissor Rock</Link>
-                </li>
-                <li>
-                    <Link to="/Tris-game" className='navbar'>Tris Game</Link>
-                </li>
-                <li>
-                    <Link to="/Secret-Number" className='navbar'>Secret Number Game</Link>
->>>>>>> 9104435ca8f9cb08a68baa91e38e66cf662e39b0
                 </li>
             </ul>
         </nav>
